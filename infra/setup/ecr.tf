993d3d6eb68950fc471f16d6fb5497fc70723264
Ram Demo <ramnarendra@yahoo.com>
2026-02-28 16:56:51 -0500

    Comming IAM changes

---
+++ infra/setup/ecr.tf
@@ -0,0 +1,24 @@
+##########
+# Create ECR (Elastic Container Registry) repos for storing Docker images #
+##########
+resource "aws_ecr_repository" "app" {
+  name                 = "receipe-app-api-app"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    # NOTE: Update to true for real deployments.
+    scan_on_push = false
+  }
+}
+
+resource "aws_ecr_repository" "proxy" {
+  name                 = "receipe-app-api-proxy"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    # NOTE: Update to true for real deployments.
+    scan_on_push = false
+  }
+}
